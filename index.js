d0d8f44aa9ab6234258433674f8cbc6bf38558e0
Sardor-creater <sardor-creater@yandex.com>
2022-02-15 19:05:22 +0300

    necala

---
+++ index.js
@@ -0,0 +1,50 @@
+widgetNameIntr = function() {
+    var widget = this;
+    this.code = null;
+
+    this.yourVar = {};
+    this.yourFunc = function() {};
+
+    // вызывается один раз при инициализации виджета, в этой функции мы вешаем события на $(document)
+    this.bind_actions = function(){
+        //пример $(document).on('click', 'selector', function(){});
+    };
+
+    // вызывается каждый раз при переходе на страницу
+    this.render = function() {
+        var bgc = $('.pipeline_wrapper').children('.pipeline_status:not(.h-hidden)').filter(
+            function(i){
+                return i==2
+            }).find('.pipeline_status__head_line').css('background-color');
+
+        $('.pipeline_wrapper').children('.pipeline_status:not(.h-hidden)').filter(
+            function(i){
+                return i==2}).find('.pipeline_status__head_title').css('color', bgc);
+    };
+
+    // вызывается один раз при инициализации виджета, в этой функции мы загружаем нужные данные, стили и.т.п
+    this.init = function(){
+
+    };
+
+    // метод загрузчик, не изменяется
+    this.bootstrap = function(code) {
+        widget.code = code;
+        // если frontend_status не задан, то считаем что виджет выключен
+        // var status = yadroFunctions.getSettings(code).frontend_status;
+        var status = 1;
+
+        if (status) {
+            widget.init();
+            widget.render();
+            widget.bind_actions();
+            $(document).on('widgets:load', function () {
+                widget.render();
+            });
+        }
+    }
+};
+// создание экземпляра виджета и регистрация в системных переменных Yadra
+// widget-name - ИД и widgetNameIntr - уникальные названия виджета
+yadroWidget.widgets['widget-name'] = new widgetNameIntr();
+yadroWidget.widgets['widget-name'].bootstrap('widget-name');
